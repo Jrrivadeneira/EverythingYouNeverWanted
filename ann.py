e189ef68ddb9e8157aa81ec4cf13c7cc193669de
Connor Flanigan <connorflanigan@gmail.com>
2016-02-01 18:52:23 -0500

    fuck you it works

--- ann.py
+++ ann.py
@@ -13,7 +13,8 @@ from neuralNetwork import *
 numHidden = 5
 percHold = .2
 
-in_handle = sys.argv[1]
+giraffe = sys.argv[1]
+
 if (len(sys.argv) > 2):
     if(sys.argv[2] == "h"):
         numHidden = sys.argv[3]
@@ -26,15 +27,33 @@ if (len(sys.argv) > 2):
             if(sys.argv[4] == "h"):
                 numHidden = sys.argv[5]
 
-dat = readData(in_handle)
-x = NeuralNetwork(2, numHidden, 1)
+# read data from file
+data = readData( giraffe)
+
+# construct the neural network with 2 input nodes, numHidden hidden nodes, and 1 output node
+net = NeuralNetwork(2, numHidden, 1)
+
+# init matrices, teachmat being the input and answerMat being the output
 teachMat = []
 answerMat = []
-for k in dat:
+
+#for each line in the input file
+for k in data:
+	# if that line is not empty
     if(k!=['']):
+		# add the first two values to a new line of teachMat and the last value to a new line of answerMat
         teachMat += [k[:2]]
         answerMat += k[2:]
+
+# create and correctly format the matrices from the arrays
 teachMat = matrix(teachMat)
 answerMat = matrix(answerMat).T
-myMat = matrix(dat)
-print x.teach(teachMat, answerMat, 0.01)
+myMat = matrix(data)
+
+# divide  teachMat and answerMat  by the holdout percentage
+holdCount = floor(percHold*200)
+net.learn(teachMat[:holdCount,:],answerMat[:holdCount],teachMat[holdCount:,:],answerMat[holdCount:],0.01)
+
+# hand these matrices to the network
+
+# print whatever the hell it is this is, which is nothing
--- neuralNetwork.py
+++ neuralNetwork.py
@@ -1,8 +1,11 @@
 """ Jack Rivadeneira 
 Connor Flannigan 
 Chris Knapp"""
+
 from numpy import *
+
 class NeuralNetwork(object):
+
 	"""docstring for NeuralNetwork"""
 	def __init__(self,NumberOfInputNodes,NumberOfHiddenNodes,NumberOfOutputNodes):
 		self.numberOfInputNodes = NumberOfInputNodes
@@ -10,6 +13,7 @@ class NeuralNetwork(object):
 		self.numberOfOutputNodes =NumberOfOutputNodes
 		self.layerOneWeights = random.rand(self.numberOfInputNodes,self.numberOfHiddenNodes) * 2 - 1
 		self.layerTwoWeights = random.rand(self.numberOfHiddenNodes,self.numberOfOutputNodes) * 2 - 1
+	
 	"""The sigmoid function for this NeuralNetwork"""
 	def sigmoid(self, t):
 		return 1 / (1 + exp( - t))
@@ -23,14 +27,6 @@ class NeuralNetwork(object):
 	
 	"""To clean Up Calculations"""
 	def deltaTwo(self,error):
-		# print self.deltaThree(error).shape
-		# print self.layerTwoWeights.T.shape
-		# print self.sigmoidPrime(self.unactivatedValuesOfLayerTwo).shape
-		# a = self.deltaThree(error)
-		# b = multiply(self.layerTwoWeights.T,self.sigmoidPrime(self.unactivatedValuesOfLayerTwo))
-		# print a.shape
-		# print b.shape
-		# return dot(a.T,b)
 		a = self.deltaThree(error)
 		b = self.layerTwoWeights.T
 		c = self.sigmoidPrime(self.unactivatedValuesOfLayerTwo)
@@ -42,40 +38,51 @@ class NeuralNetwork(object):
 	def predict(self,inputMatrix):
 		self.unactivatedValuesOfLayerTwo = dot(inputMatrix,self.layerOneWeights)
 		self.activatedValuesOfLayerTwo = self.sigmoid(self.unactivatedValuesOfLayerTwo)
+		
 		self.unactivatedValuesOfLayerThree = dot(self.activatedValuesOfLayerTwo,self.layerTwoWeights)
 		self.activatedValuesOfLayerThree = self.sigmoid(self.unactivatedValuesOfLayerThree)
+		
 		return self.activatedValuesOfLayerThree
 	
 	"""U GOn L3rn2DAY"""
 	def teach(self,inputMatrix,answersMatrix,alpha):
+		
 		error = (answersMatrix - self.predict(inputMatrix))
 		acceptableError = 1e-8
+		
 		while(error.mean()>acceptableError):
-			print error.mean()
 			derivativeOfOutputWithRespectToWeightTwo = dot(self.activatedValuesOfLayerTwo.T,self.deltaThree(error))
 			derivativeOfOutputWithRespectToWeightOne = dot(inputMatrix.T, self.deltaTwo(error))
 			self.layerOneWeights -= alpha*derivativeOfOutputWithRespectToWeightOne
 			self.layerTwoWeights -= alpha*derivativeOfOutputWithRespectToWeightTwo
 			error = (answersMatrix - self.predict(inputMatrix))
-
-"""------------------TestArea------------------"""
-from dataReader import *
-dat = readData("testFile.csv")
-x = NeuralNetwork(2,4,1)
-# print x.numberOfHiddenNodes
-# print x.layerOneWeights
-# print x.layerTwoWeights
-teachMat = []
-answerMat = []
-for k in dat:
-	if(k!=['']):
-		teachMat += [k[:2]]
-		answerMat += k[2:]
-teachMat = matrix(teachMat)
-answerMat = matrix(answerMat).T
-myMat = matrix(dat)
-# print answerMat
-print x.teach(teachMat,answerMat,0.01)
-# print x.predict(random.rand(5,2))
-
-
+			
+	# l3Rn $uM $Hap3s
+	def learn(self, inputMatrix, answersMatrix, testMat, testAns,alpha):
+		
+		bestNet = self
+		bestError = (testAns - self.predict(testMat)).mean()
+		
+		print bestError
+		
+		error = (answersMatrix - self.predict(inputMatrix))
+		acceptableError = 1e-8
+		print error.mean()
+		
+		while(abs(error.mean())>acceptableError):
+			derivativeOfOutputWithRespectToWeightTwo = dot(self.activatedValuesOfLayerTwo.T,self.deltaThree(error))
+			derivativeOfOutputWithRespectToWeightOne = dot(inputMatrix.T, self.deltaTwo(error))
+			self.layerOneWeights -= alpha*derivativeOfOutputWithRespectToWeightOne
+			self.layerTwoWeights -= alpha*derivativeOfOutputWithRespectToWeightTwo
+			
+			testErr = testAns - self.predict(testMat)
+			if (abs(testErr.mean()) < bestError):
+				bestError = testErr.mean()
+				bestNet = self
+				
+			error = (answersMatrix - self.predict(inputMatrix).round())
+		
+		print bestError
+		print error.mean()
+		
+		return bestNet
