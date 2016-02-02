bf40b1c3a65755c74806ec6dee1d25cef8e450f7
Chris Knapp <crknapp@wpi.edu>
2016-02-01 19:14:10 -0500

    Fuck Python

--- ann.py
+++ ann.py
@@ -16,16 +16,17 @@ percHold = .2
 giraffe = sys.argv[1]
 
 if (len(sys.argv) > 2):
-    if(sys.argv[2] == "h"):
-        numHidden = sys.argv[3]
+    print sys.argv
+    if(sys.argv[2] == 'h'):
+        numHidden = int(sys.argv[3])
         if(len(sys.argv) > 4):
-            if(sys.argv[4] == "p"):
-                percHold = sys.argv[5]
-    if(sys.argv[2] == "p"):
-        percHold = sys.argv[3]
+            if(sys.argv[4] == 'p'):
+                percHold = float(sys.argv[5])
+    if(sys.argv[2] == 'p'):
+        percHold = float(sys.argv[3])
         if(len(sys.argv) > 4):
-            if(sys.argv[4] == "h"):
-                numHidden = sys.argv[5]
+            if(sys.argv[4] == 'h'):
+                numHidden = int(sys.argv[5])
 
 # read data from file
 data = readData( giraffe)
